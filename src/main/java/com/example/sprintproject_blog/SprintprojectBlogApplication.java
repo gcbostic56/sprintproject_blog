a5c22a9adb8af6168940303877ddaa7900de5df4
Cameron <bmscameronb072a@yahoo.com>
2022-06-09 09:32:38 -0400

    Project Setup Commit - For Cloning

---
+++ src/main/java/com/example/sprintproject_blog/SprintprojectBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintproject_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintprojectBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintprojectBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sprintproject_blog/SprintprojectBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintproject_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintprojectBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
